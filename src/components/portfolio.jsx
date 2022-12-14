9dacdbf2dc50896c721258355876f3e38441b95d
Oliver Hunter <hunter.oliver0512@protonmail.com>
2022-12-14 16:28:01 +0000

    Add 'Portfolio' structure

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Portfolio() {
     return (
-        <div></div>
+        <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                Portfolio here...
+            </div>
+        </div>
     )
 }
 
