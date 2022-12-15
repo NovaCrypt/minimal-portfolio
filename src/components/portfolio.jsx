b3f07e919e650d7ffac957408d49891efe71c204
Oliver Hunter <hunter.oliver0512@protonmail.com>
2022-12-15 16:25:31 +0000

    debug images

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -8,7 +8,7 @@ function Portfolio() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {portfolio.map(project => (
                     <PortfolioItem 
-                        imgUrl={project.imgUrl}
+                        imgURL={project.imgURL}
                         title={project.title}
                         stack={project.stack}
                         link={project.link}
