706615628f8867be3f9201477352c69306433158
Oliver Hunter <hunter.oliver0512@protonmail.com>
2022-12-15 13:49:30 +0000

    portfolio build

---
+++ src/components/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'GitHub Profile',
+        imgURL: '',
+        stack: ['Markdown', 'GitHub'],
+        link: 'https://www.github.com/NovaCrypt'
+    },
+    {
+        title: 'Minimal Portfolio',
+        imgURL: '',
+        stack: ['Vite', 'Tailwind', 'ReactJS'],
+        link: ''
+    },
+    {
+        title: 'Patreon',
+        imgURL: '',
+        stack: ['Graphic Design', 'AI Generation'],
+        link: ''
+    }
+]
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import portfolio from "./portfolio";
+import PortfolioItem from "./portfolioItem";
 
 function Portfolio() {
     return (
         <div className="flex flex-col md:flex-row items-center justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                Portfolio here...
+                {portfolio.map(project => (
+                    <PortfolioItem 
+                        imgUrl={project.imgUrl}
+                        title={project.title}
+                        stack={project.stack}
+                        link={project.link}
+                    />
+                ))}
             </div>
         </div>
     )
