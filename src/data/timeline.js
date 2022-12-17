9fd8b7ebddd702aebd0e2bc9237d66bad9e4e590
NovaCrypt <hunter.oliver0512@protonmail.com>
2022-12-17 10:36:41 +0000

    Filled out the Timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -5,5 +5,26 @@ export default [
         duration: '1 year',
         details:
             'My goal is to fit my work and make a living in between my studies at a full time education while I increase my employability by getting more qualificatiosns. That\' the main purpose of going freelance.'
+    },
+    {
+        year: '2022',
+        title: 'First Artistic Project',
+        duration: '3 months',
+        details: 
+            'It was in 2022, that I began my journey into the artistic world of Graphic Design with the intention of expanding my abilities to building the full branding experience, inc. Logos, Colour Schemes, Layouts & Typography.'
+    },
+    {
+        year: '2021',
+        title: 'Front-End Web Developer',
+        duration: '6 months',
+        details: 
+            'In 2021, I was hired by In & Beyond Bath Ltd. to rebrand and publish their website on a 6 month contract. During this time, I also ensured the website was responsive and quick to load, providing the company with high scores on Google\'s search engine.'
+    },
+    {
+        year: '2016',
+        title: 'First Line of Code',
+        duration: 'The Beginning',
+        details:
+            'In 2016, I wrote my first real line of code, beyond just childish messing around. After years of being fascinated with the concept of "computer language", it was in secondary education that I realised I could make this my career...'
     }
 ]
