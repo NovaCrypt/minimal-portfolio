765dcfab13601846488c70801b1b1e2278c41f0a
NovaCrypt <hunter.oliver0512@protonmail.com>
2022-12-17 10:42:29 +0000

    Style fixes

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -5,19 +5,19 @@ import Title from "./title";
 
 function Timeline() {
     return (
-        <div className="flex flex-col md:flex-row justify-center my-20 w-full md:w-7/12">
+        <div className="flex flex-col md:flex-row justify-center my-20">
             <div className="w-full md:w-7/12">
                 <Title>Timeline</Title>
                 {timeline.map(item => (
-                    <TimelineItem
-                        year={item.year}
-                        title={item.title}
-                        duration={item.duration}
-                        details={item.details}
-                    />
+                <TimelineItem 
+                    year={item.year}
+                    title={item.title}
+                    duration={item.duration}
+                    details={item.details}
+                />
                 ))}
             </div>
-        </div>
+      </div>
     )
 }
 
