3b8a2b0753d010810d96db9c07ad3be0b2f36a8a
NovaCrypt <hunter.oliver0512@protonmail.com>
2022-12-17 15:54:43 +0000

    Fix typo in timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,7 +4,7 @@ export default [
         title: 'Freelance Web Developer',
         duration: '1 year',
         details:
-            'My goal is to fit my work and make a living in between my studies at a full time education while I increase my employability by getting more qualificatiosns. That\' the main purpose of going freelance.'
+            'My goal is to fit my work and make a living in between my studies at a full time education while I increase my employability by getting more qualifications. That\'s the main purpose of going freelance.'
     },
     {
         year: '2022',
