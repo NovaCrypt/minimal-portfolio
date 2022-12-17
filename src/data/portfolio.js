93f287c522fa3d663075205263f9bc9d484e114a
NovaCrypt <hunter.oliver0512@protonmail.com>
2022-12-17 12:11:29 +0000

    fix file paths for deployment

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'GitHub Profile',
-        imgURL: '/assets/Screenshot 2022-12-17 111303.png',
+        imgURL: 'assets/Screenshot 2022-12-17 111303.png',
         stack: ['Markdown', 'GitHub'],
         link: 'https://www.github.com/NovaCrypt'
     },
     {
         title: 'Minimal Portfolio',
-        imgURL: '/assets/Screenshot 2022-12-17 104328.png',
+        imgURL: 'assets/Screenshot 2022-12-17 104328.png',
         stack: ['Vite', 'Tailwind', 'ReactJS'],
         link: 'https://www.github.com/NovaCrypt/minimal-'
     },
     {
         title: 'Patreon',
-        imgURL: '/assets/Screenshot 2022-12-15 155919.png',
+        imgURL: 'assets/Screenshot 2022-12-15 155919.png',
         stack: ['Graphic Design', 'AI Generation'],
         link: 'https://www.patreon.com/NovaAI'
     }
