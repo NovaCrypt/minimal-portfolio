9a5e417e610b183ca49524866a3865c22ac41591
NovaCrypt <hunter.oliver0512@protonmail.com>
2022-12-17 08:23:25 +0000

    Boilerplating the timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./timelineItem";
 
 function Timeline() {
     return (
-        <div></div>
+        <div className="flex flex-col md:flex-row justify-center my-20 w-full md:w-7/12">
+            <div className="w-full md:w-7/12">
+                {timeline.map(item => (
+                    <TimelineItem
+                        year={item.year}
+                        title={item.title}
+                        duration={item.duration}
+                        details={item.details}
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
 
---
+++ src/data/timeline.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Freelance Web Developer',
+        duration: '1 year',
+        details:
+            'My goal is to fit my work and make a living in between my studies at a full time education while I increase my employability by getting more qualificatiosns. That\' the main purpose of going freelance.'
+    }
+]
